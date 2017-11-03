1a2f6a41036ec6626a0784b3284c61695de879cc
elonder629 <ericlonder730@gmail.com>
2017-11-03 08:46:38 -0400

    Initial commit

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by el693 on 11/3/17.
+ */
+public class HighSchoolClass {
+
+        private Student [] students;
+        //constructors and other data fields not shown
+//returns the valedictorian of the class
+        public Student getValedictorian()
+        {
+
+        }
+        // returns the percentage of students in the
+// honors program
+        public double getHonorsPercentage()
+        {
+// to be implemented
+        }
+    }
+
---
+++ src/com/company/Student.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by el693 on 11/3/17.
+ */
+public class Student {
+
+}
